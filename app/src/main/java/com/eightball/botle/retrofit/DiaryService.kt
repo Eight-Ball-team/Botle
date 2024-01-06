6805725fae69303477ef9998e4a66ed3d3884992
수잉 <djswpsrkaksskfskf@gmail.com>
2024-01-07 03:53:35 +0900

    apiservice

--- app/src/main/java/com/eightball/botle/retrofit/DiaryService.kt
+++ app/src/main/java/com/eightball/botle/retrofit/DiaryService.kt
@@ -1,4 +1,19 @@
 package com.eightball.botle.retrofit
 
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface DiaryService {
+    @POST("/diary/write")
+    fun submitDiray() : Call<>
+
+    @POST("/diary/modify")
+    fun modifyDiary() : Call<>
+
+    @GET("/diary/search/{sort}/{category}")
+    fun checkOneDiary() : Call<>
+
+    @GET("/diarys/search/{sort}/{category}")
+    fun checkDiarys() : Call<>
 }
